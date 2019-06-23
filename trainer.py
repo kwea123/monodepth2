c71e08d0c6b64b4c3bf928c1263561c4b219b8d2
kwea123 <kwea123@gmail.com>
2019-06-23 23:12:33 +0900

    add mask to reconstruction loss

--- trainer.py
+++ trainer.py
@@ -109,7 +109,10 @@ class Trainer:
 
         # data
         datasets_dict = {"kitti": datasets.KITTIRAWDataset,
-                         "kitti_odom": datasets.KITTIOdomDataset}
+                         "kitti_odom": datasets.KITTIOdomDataset,
+                         "apollo": datasets.ApolloDataset,
+                         "city": datasets.CityscapesDataset
+                         }
         self.dataset = datasets_dict[self.opt.dataset]
 
         fpath = os.path.join(os.path.dirname(__file__), "splits", self.opt.split, "{}_files.txt")
